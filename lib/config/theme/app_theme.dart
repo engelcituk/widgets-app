849717b3ba15c6809291825a99afaa9d02ee664c
cituk <ecituk@thefives.com.mx>
2023-07-12 23:10:28 -0500

    Opciones de nuestra aplicación

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -24,7 +24,10 @@ class AppTheme {
 
   ThemeData getTheme() => ThemeData(
     useMaterial3: true,
-    colorSchemeSeed: colorList[ selectedColor ]
+    colorSchemeSeed: colorList[ selectedColor ],
+    appBarTheme: const AppBarTheme(
+      centerTitle: false,
+    )
   );
 
 }
