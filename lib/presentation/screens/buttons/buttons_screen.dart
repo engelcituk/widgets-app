d0128ca4546d337e3b052a8184a3957c0fe620fd
cituk <ecituk@thefives.com.mx>
2023-07-13 23:34:54 -0500

    Diferentes botones pre-configurados

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtonsScreen extends StatelessWidget {
   // esto para que en el router pueda hacer esto  name: ButtonsScreen.name, y no estar haciendo instancias de ButtonsScreen
@@ -12,7 +13,73 @@ class ButtonsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Buttons Screen'),
       ),
-      body: const Placeholder(),
+      body: const _ButtonsView(),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon( Icons.arrow_back_ios_new_rounded),
+        onPressed: () {
+          context.pop(); // para salir de la ruta
+        },
+      ),
+    );
+  }
+}
+
+class _ButtonsView extends StatelessWidget {
+  const _ButtonsView();
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: Wrap(
+          spacing: 10,
+          alignment: WrapAlignment.center,
+          children: [
+            ElevatedButton(onPressed: () {}, child: const Text('Texto elevated')),
+            const ElevatedButton(onPressed: null, child: Text('Elevated Disabled')),
+            ElevatedButton.icon(
+              onPressed: (){}, 
+              icon: const Icon( Icons.favorite, color: Colors.red), 
+              label: const Text('Elevated Icon')
+            ),
+            FilledButton(onPressed: (){}, child: const Text('Filled')),
+            FilledButton.icon(
+              onPressed: (){}, 
+              icon: const Icon( Icons.favorite),
+              label: const Text('Filled icon')
+            ),
+            OutlinedButton(
+              onPressed: (){},
+              child: const Text('Outlined')
+            ),
+            OutlinedButton.icon(
+              onPressed: (){}, 
+              icon: const Icon( Icons.favorite, color: Colors.red),
+              label: const Text('OutlinedButton icon')
+            ),
+            TextButton(onPressed: (){}, child: const Text('Text button') ),
+            TextButton.icon(
+              onPressed: (){},
+              icon: const Icon( Icons.favorite, color: Colors.red),
+              label: const Text('Text button Icon')
+            ),
+            IconButton(onPressed: (){}, icon: const Icon(Icons.app_registration_rounded)),
+            IconButton(
+              onPressed: (){},
+              icon: const Icon(Icons.app_registration_rounded),
+              style: ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll( colors.primary ),
+                iconColor: const MaterialStatePropertyAll( Colors.white )
+              ),
+            )
+
+          ],
+        ),
+      ),
     );
   }
 }
