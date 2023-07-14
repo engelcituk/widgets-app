94baebfc56f8f2c8407fc144997bb67bf45f557c
cituk <ecituk@thefives.com.mx>
2023-07-13 23:53:51 -0500

    Botón personalizado

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -29,6 +29,7 @@ class _ButtonsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     final colors = Theme.of(context).colorScheme;
     
     return SizedBox(
@@ -67,6 +68,7 @@ class _ButtonsView extends StatelessWidget {
               icon: const Icon( Icons.favorite, color: Colors.red),
               label: const Text('Text button Icon')
             ),
+            const CustomButton(),
             IconButton(onPressed: (){}, icon: const Icon(Icons.app_registration_rounded)),
             IconButton(
               onPressed: (){},
@@ -78,6 +80,32 @@ class _ButtonsView extends StatelessWidget {
             )
 
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {
+            
+          },
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text('Hola mundo', style: TextStyle(color: Colors.white),),
+          ),
         ),
       ),
     );
