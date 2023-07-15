d1b8eda896e2fbb797414d71289fe1c35aa8ffa0
cituk <ecituk@thefives.com.mx>
2023-07-14 23:45:37 -0500

    Linear y Circular progress controlados

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -30,7 +30,7 @@ class _ProgressView extends StatelessWidget {
           SizedBox(height:10 ,),
           CircularProgressIndicator(strokeWidth: 2, backgroundColor: Colors.black45,),
           SizedBox(height:10 ,),
-          Text('Progress Indicator Controlado'),
+          Text('Circular Linear Controlado'),
           SizedBox(height:10 ),
           _ControlledProgressIndicator()
         ],
@@ -44,6 +44,27 @@ class _ControlledProgressIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return StreamBuilder(
+      stream: Stream.periodic( const Duration(milliseconds: 300), (value){
+        return value * 2 / 100;
+      }).takeWhile(( value ) => value <= 1 ),
+      builder: (context, snapshot) {
+        final progressValue = snapshot.data ?? 0;
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(value: progressValue, strokeWidth: 2, backgroundColor: Colors.black12,),
+              const SizedBox(width:20 ),
+              Expanded(
+                child: LinearProgressIndicator(value: progressValue,)
+              )
+      
+            ],
+          ),
+        );
+      },
+    );
   }
 }
